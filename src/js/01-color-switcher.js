6de9fdf8c615c268290d62a39c64835ee6a5fd14
OksanaShyian <108744464+OksanaShyian@users.noreply.github.com>
2022-12-05 19:24:42 +0200

    update file

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let timeId = null;
+
+startButton.addEventListener('click', onButtonStart);
+stopButton.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  timerId = setInterval(
+    () => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+    },
+    1000,
+    startButton.setAttribute('disabled', true)
+  );
+}
+function onButtonStop() {
+  clearInterval(timerId);
+  startButton.removeAttribute('disabled', true);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
